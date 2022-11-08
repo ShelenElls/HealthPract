8f6f7833aa572c36051a6341fd7ebc858afd78b6
shelen ells <shelen.ells@gmail.com>
2022-11-08 13:34:18 -0700

    updates to utilities and api folders

--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,23 @@ const app = require('express')();
 
 const {
     getAllTodos,
-    postOneTodo
+    postOneTodo,
+    deleteTodo,
+    editTodo,
 } = require('./APIs/todos')
 
 
 app.post('/todo', postOneTodo);
 app.get('/todos', getAllTodos);
+app.delete('/todo/:todoId', deleteTodo);
+
 exports.api = functions.https.onRequest(app);
+
+const {
+    loginUser,
+    signUpUser
+} = require('./APIs/users')
+
+// Users
+app.post('/login', loginUser);
+app.post('/signup', signUpUser);
