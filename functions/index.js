5e299673038136bcd1ae39bba67c5b2d226b515e
shelen ells <shelen.ells@gmail.com>
2022-11-08 11:53:24 -0700

    viewable localhost 5001

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+
+
+const functions = require('firebase-functions');
+const app = require('express')();
+
+const {
+    getAllTodos,
+    postOneTodo
+} = require('./APIs/todos')
+
+
+app.post('/todo', postOneTodo);
+app.get('/todos', getAllTodos);
+exports.api = functions.https.onRequest(app);
--- plan.md
+++ plan.md
@@ -9,3 +9,6 @@ expo
 
 Firebase
 <!--   https://www.learnhowtoprogram.com/react-part-time-react-track/react-with-nosql-part-1/adding-firebase-to-react    -->
+
+<!-- firebase API needs to be set up with the pay as you go plan in order to deploy the project  -->
+
